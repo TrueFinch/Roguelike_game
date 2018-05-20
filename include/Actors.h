2e208d6c46cfdf0032f147efe23c1ffde3dfdefd
truefinch <glushkov.vk@students.dvfu.ru>
2018-05-21 04:25:00 +1000

    Add class Actor, Hero, Wall

--- include/Actors.h
+++ include/Actors.h
@@ -5,7 +5,7 @@
 #ifndef ROGUELIKE_ACTOR_H
 #define ROGUELIKE_ACTOR_H
 
-namespace actor {
+namespace actors {
 
 class Actor;
 class ActiveActor;
@@ -13,11 +13,7 @@ class Hero;
 class Princess;
 class Zombie;
 class Dragon;
-class PassiveActor;
 class Wall;
-class Potion;
-class HealthP;
-class ManaP;
 
 
 #define EPS 2e-15
@@ -33,32 +29,54 @@ struct Point {
 class Actor {
  public:
   Actor() = default;
+  virtual void setCoord(Point) = 0;
   virtual Point getCoord() const = 0;
-  virtual void setCoord() = 0;
+  virtual void setSymbol(char) = 0;
+  virtual char getSymbol() const = 0;
+  virtual void setMaxHP(int) = 0;
+  virtual int getMaxHP() const = 0;
+  virtual void setCurHP(int) = 0;
+  virtual int getCurHP() const = 0;
+  virtual void setDamagePoints(int) = 0;
+  virtual int getDamagePoints() = 0;
   virtual bool isDead() const = 0;
-  virtual Point move() = 0;
-  virtual void collide(const Actor&) = 0;
-  virtual void collide(const ActiveActor&) = 0;
-  virtual void collide(const Hero&) = 0;
-  virtual void collide(const Princess&) = 0;
-  virtual void collide(const Zombie&) = 0;
-  virtual void collide(const Dragon&) = 0;
-  virtual void collide(const PassiveActor&) = 0;
-  virtual void collide(const Wall&) = 0;
-  virtual void collide(const Potion&) = 0;
-  virtual void collide(const HealthP&) = 0;
-  virtual void collide(const ManaP&) = 0;
- private:
-  int damage_points_;
-  int cur_health_points_;
-  int max_health_points_;
-  int cur_mana_points_;
-  int max_mana_points_;
-  char symbol_;
-  bool is_dead_;
-  Point coord_;
+  virtual void collide(Actor&) = 0;
+  virtual void collide(Hero&) = 0;
+//  virtual void collide(const Princess&) = 0;
+//  virtual void collide(const Zombie&) = 0;
+//  virtual void collide(const Dragon&) = 0;
+//  virtual void collide(const PassiveActor&) = 0;
+  virtual void collide(Wall&) = 0;
+//  virtual void collide(const Potion&) = 0;
+//  virtual void collide(const HealthP&) = 0;
+//  virtual void collide(const ManaP&) = 0;
+ protected:
+  int damage_points_  = 0;
+  int cur_mana_points_ = 0;
+  int max_mana_points_ = 0;
+  int cur_health_points_ = 0;
+  int max_health_points_ = 0;
+  char symbol_ = '$';
+  bool is_dead_ = false;
+  bool is_immortal_  = false;
+  Point coord_ = {0, 0};
 };
 
-} // namespace actor
+class Hero : Actor {
+ public:
+  Hero() = default;
+  Hero(int damage, int mana, int health, Point coord);
+  void setCoord(Point) override;
+  void collide(Actor&) override;
+  void collide(Hero&) override;
+  void collide(Wall&) override;
+};
+
+class Wall : Actor {
+ public:
+  Wall() = default;
+  Wall(int health, Point coord);
+};
+} // namespace actors
 
 #endif //ROGUELIKE_ACTOR_H
--- source/Actors.cpp
+++ source/Actors.cpp
@@ -5,17 +5,49 @@
 #include <Actors.h>
 #include <cmath>
 
-actor::Point::Point() {
+actors::Point::Point() {
   this->x = 42.0;
   this->y = 42.0;
 }
 
-actor::Point::Point(double x, double y) : x{x}, y{y} {}
+actors::Point::Point(double x, double y) : x{x}, y{y} {}
 
-const bool actor::Point::operator<(const Point& rhs) const {
+const bool actors::Point::operator<(const Point& rhs) const {
   return ((this->x < rhs.x) && (this->y < rhs.y));
 }
 
-const bool actor::Point::operator==(const Point& rhs) const {
+const bool actors::Point::operator==(const Point& rhs) const {
   return ((fabs(this->x - rhs.x) <= EPS) && (fabs(this->y - rhs.y) <= EPS));
+}
+
+actors::Hero::Hero(int damage, int mana, int health, actors::Point coord) {
+  damage_points_ = damage;
+  max_mana_points_ = mana;
+  cur_mana_points_ = mana;
+  max_health_points_ = health;
+  cur_health_points_ = health;
+  coord_ = coord;
+  symbol_ = '@';
+  is_dead_ = false;
+  is_immortal_ = false;
+}
+
+void actors::Hero::setCoord(Point point) {
+  coord_ = point;
+}
+
+void actors::Hero::collide(actors::Actor& _ca) {
+  _ca.collide(*this);
+}
+
+void actors::Hero::collide(actors::Wall& _cw) {
+  _cw.setCurHP(_cw.getCurHP() - getDamagePoints());
+}
+
+actors::Wall::Wall(int health, actors::Point coord) {
+  max_health_points_ = health;
+  cur_health_points_ = health;
+  coord_ = coord;
+  is_dead_ = false;
+  is_immortal_ = false;
 }
