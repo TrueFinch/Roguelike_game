c2e1429cad5003efc3d89736d2db46b7ae7eb26e
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-05-24 11:45:53 +1000

    Add map loader and create first try of level1.txt

--- include/Map.h
+++ include/Map.h
@@ -8,28 +8,29 @@
 #include <Actors.h>
 #include <vector>
 #include <cstring>
+#include <memory>
 
 namespace map {
 
 class Cell {
  public:
   Cell() = default;
-  Cell(actors::Point coord, actors::Actor& ref = void);
-  void setReference(actors::Actor&);
-  actors::Actor& getReference() const;
+  Cell(actors::Point coord, std::shared_ptr<actors::Actor> ptr = nullptr);
+  void setPointer(std::shared_ptr<actors::Actor>);
+  std::shared_ptr<actors::Actor> getPointer() const;
+ private:
+  actors::Point coord_;
+  std::shared_ptr<actors::Actor> ptr_;
 };
 
 class Map {
  public:
   Map() = default;
-  Map(std::vector<actors::Actor&> actors, int rows, int cols, actors::Point player_spawn);
-  void move(const actors::Point& from, const actors::Point& to);
+  void loadMap();
   std::vector<std::string> getArea(const actors::Point& coord, const int radius) const;
-  actors::Actor& getActor(const actors::Point&) const;
  private:
-//  std::vector<actors::Actor&> actors_;
+  std::vector<std::shared_ptr<actors::Actor>> actors_;
   std::vector<std::vector<Cell>> board_;
-  actors::Point player_spawn_;
 };
 
 } // namespace map
--- source/Map.cpp
+++ source/Map.cpp
@@ -2,3 +2,60 @@
 // Created by truefinch on 21.05.18.
 //
 
+#include <Map.h>
+#include <utility>
+
+map::Cell::Cell(actors::Point coord, std::shared_ptr<actors::Actor> ptr = nullptr) : coord_{coord},
+                                                                                     ptr_{std::move(ptr)} {}
+
+void map::Cell::setPointer(std::shared_ptr<actors::Actor> ptr) {
+  ptr_ = std::move(ptr);
+}
+
+std::shared_ptr<actors::Actor> map::Cell::getPointer() const {
+  return ptr_;
+}
+
+void map::Map::loadMap() {
+  FILE* map = fopen("./map/level1.txt", "rt");
+  int rows = 0, cols = 0;
+  char c;
+  std::shared_ptr<actors::Actor> actor_ptr;
+  fscanf(map, "%i\n%i\n", &rows, &cols);
+
+  for (int i = 0; i < rows; ++i) {
+    std::vector<Cell> tmp((unsigned long) (cols));
+    for (int j = 0; i < cols; ++j) {
+      fscanf(map, "%c", &c);
+      switch (c) {
+        case '@':
+          actor_ptr = actors::Actor::createActor(actors::HERO_ID, {{'D', 100},
+                                                                   {'M', 100},
+                                                                   {'H', 100},
+                                                                   {'R', i},
+                                                                   {'C', j}});
+          break;
+        case 'z':
+          actor_ptr = actors::Actor::createActor(actors::ZOMBIE_ID, {{'D', 100},
+                                                                     {'H', 100},
+                                                                     {'R', i},
+                                                                     {'C', j}});
+          break;
+        case '#':
+          actor_ptr = actors::Actor::createActor(actors::WALL_ID, {{'H', 100},
+                                                                   {'R', i},
+                                                                   {'C', j}});
+          break;
+        case '.':
+          actor_ptr = nullptr;
+          break;
+      }
+      if (actor_ptr != nullptr) {
+        actors_.push_back(actor_ptr);
+      }
+      tmp.emplace_back({{i, j}, actor_ptr});
+    }
+    board_.push_back(tmp);
+    fscanf(map, "\n");
+  }
+}
