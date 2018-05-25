bffa76fe855f35f6deaa23a9f1d967d4ef1035b3
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-05-25 22:40:16 +1000

    Add ability to resize terminal window - now the interface if resizing too

--- source/Game.cpp
+++ source/Game.cpp
@@ -9,39 +9,46 @@
 game_space::Game::Game(int rows, int cols) : rows_{rows}, cols_{cols} {
   initscr();
   clear();
+//  raw();
+  cbreak();
   keypad(stdscr, true);
   noecho();
   curs_set(0);
+  halfdelay(1);
   resize_term(rows, cols);
 
   game_state_ = ui::LOADING;
   loading_ = ui::Loading();
   main_menu_ = ui::MainMenu();
   map_ = map::Map();
-  map_.loadMap();
+//  map_.loadMap();
 
 }
 
 void game_space::Game::start() {
-  int key = -1;
+  int key = ERR;
   game_state_ = loading_.update(key);
 
   while (game_state_ != ui::EXIT) {
     key = getch();
     switch (game_state_) {
-      case ui::LOADING:loading_.update(key);
+      case ui::LOADING:game_state_ = loading_.update(key);
+      if (game_state_ == ui::MAIN_MENU) {
+        main_menu_.update(-1);
+      }
         break;
-      case ui::MAIN_MENU:main_menu_.update(key);
+      case ui::MAIN_MENU:game_state_ = main_menu_.update(key);
         break;
-      case ui::GAME_FIELD:break;
       case ui::SETTINGS:break;
       case ui::EXIT:finish();
         break;
+      case ui::GAME_FIELD:break;
     }
   }
 }
 
 void game_space::Game::finish() {
+  clear();
   endwin();
 }
 
