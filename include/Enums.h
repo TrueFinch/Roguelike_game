88c898cfe5dbdc19badc90b45b08912bc7366018
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-07-08 00:16:26 +1000

    Refactor code

--- include/Enums.h
+++ include/Enums.h
@@ -5,8 +5,18 @@
 
 namespace enums {
 
-  enum ActorID {HERO_ID = '@', ZOMBIE_ID = 'Z', WALL_ID = '#', PRINCESS_ID = 'P', DRAGON_ID = 'D', NOTHING_ID = '.'};
-  enum CollideEvent {DEATH};
+enum ActorID {
+  HERO_ID = '@',
+  ZOMBIE_ID = 'Z',
+  WALL_ID = '#',
+  PRINCESS_ID = 'P',
+  DRAGON_ID = 'D',
+  NOTHING_ID = '.',
+  FIRE_BALL_ID = '*',
+  HP_POTION = 'H',
+  MP_POTION = 'M'
+};
+enum MoveEvent { MOVED = 0, NOT_MOVED = 0};
 
-  enum GameState { LOADING = 0, MAIN_MENU, SETTINGS, EXIT, GAME_FIELD, PAUSE };
+enum GameState { LOADING = 0, MAIN_MENU, SETTINGS, EXIT, GAME_FIELD, PAUSE };
 }; // namespace game_screen
