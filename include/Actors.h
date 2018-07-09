66a1271e098eaf1eb07b6b08ce0e78653acf1e1b
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-07-09 20:52:52 +1000

    Rewrite all classes because I can and I want.

--- include/Actors.h
+++ include/Actors.h
@@ -1,12 +1,13 @@
 //
 // Created by truefinch on 20.05.18.
+// Without a clear specification
+// Result becomes a mortification
 //
-
-#ifndef ROGUELIKE_ACTOR_H
-#define ROGUELIKE_ACTOR_H
+#pragma once
 
 #include <memory>
 #include <string>
+#include <utility>
 #include <Statistics.h>
 #include <Point.h>
 #include <Config.h>
@@ -16,137 +17,221 @@ namespace actor {
 
 class Actor;
 
+class ActiveActor;
+
 class Hero;
 
 class Zombie;
-
-class Wall;
 
 class Princess;
 
 class Dragon;
 
+class SpellActor;
+
+class Fireball;
+
+class PassiveActor;
+
+class Wall;
+
+class Floor;
+
+class CollectableActor;
+
+class HealthPotion;
+
+class ManaPotion;
+
 class Actor {
  public:
-  Actor() = default;
-  virtual void collide(Actor&) = 0;
-  virtual void collide(Hero&) = 0;
-  virtual void collide(Zombie&) = 0;
-  virtual void collide(Wall&) = 0;
-  virtual void collide(Princess&) = 0;
-  virtual void collide(Dragon&) = 0;
-//  virtual void collide(const Potion&) = 0;
-//  virtual void collide(const HealthP&) = 0;
-//  virtual void collide(const ManaP&) = 0;
-  virtual std::string sayName() const = 0;
-  virtual enums::ActorID sayID() const = 0;
-//  virtual char getDeadSymbol() const = 0;
-//  virtual char getUndeadSymbol() const = 0;
-//  virtual bool isDead() const = 0;
-  static std::shared_ptr<actor::Actor> createActor(enums::ActorID, config::Config&, Point);
+  Actor(Point position, bool is_immortal, std::string name, enums::ActorID id)
+      : pos_{position}, is_immortal_{is_immortal}, name_{std::move(name)}, id_{id} {};
   virtual ~Actor() = default;
+
+  virtual enums::CollideResult collide(Actor&) = 0;
+  virtual enums::CollideResult collide(ActiveActor&) = 0;
+  virtual enums::CollideResult collide(SpellActor&) = 0;
+  virtual enums::CollideResult collide(PassiveActor&) = 0;
+  virtual enums::CollideResult collide(CollectableActor&) = 0;
+
+  void setPosition(Point);
+  Point getPosition() const;
+  void setSymbol(char);
+  char getSymbol() const;
+  void setIsImmortal(bool);
+  bool getIsImmortal() const;
+  std::string getName() const;
+  enums::ActorID getID() const;
+
+ protected:
+  Point pos_;
+  bool is_immortal_;
+  std::string name_;
+  enums::ActorID id_;
 };
 
-class Hero : public stats::HeroStat, public Actor {
+class ActiveActor : public Actor {
  public:
-  Hero() = default;
-  explicit Hero(std::shared_ptr<stats::HeroStat>, Point);
-  void collide(Actor&) override;
-  void collide(Hero&) override;
-  void collide(Zombie&) override;
-  void collide(Wall&) override;
-  void collide(Princess&) override;
-  void collide(Dragon&) override;
-  std::string sayName() const override;
-  enums::ActorID sayID() const override;
- private:
-  const std::string NAME = "Hero";
-  const enums::ActorID ID = enums::HERO_ID;
+  ActiveActor(Point position, bool is_dead, bool is_immortal, std::string name,
+              enums::ActorID id, int hp, int mp, int dp, int vp, int lp)
+      : Actor(position, is_immortal, std::move(name), id),
+        is_dead_{is_dead},
+        max_health_points_{hp},
+        cur_health_points_{hp},
+        max_mana_points_{mp},
+        cur_mana_points_{mp},
+        max_damage_points_{dp},
+        cur_damage_points_{dp},
+        max_visibility_points_{vp},
+        cur_visibility_points_{vp},
+        max_level_points_{lp},
+        cur_level_points_{lp} {};
+  ~ActiveActor() override = default;
+
+  enums::CollideResult collide(Actor&) override;
+  virtual enums::CollideResult collide(ActiveActor&) = 0;
+  enums::CollideResult collide(PassiveActor&) override;
+  enums::CollideResult collide(SpellActor&) override;
+  enums::CollideResult collide(CollectableActor&) override;
+
+  void setIsDead(bool);
+  bool getIsDead() const;
+
+  static std::shared_ptr<actor::ActiveActor> createActor(enums::ActorID, config::Config&, Point);
+
+ protected:
+  bool is_dead_;
+  int max_health_points_;
+  int cur_health_points_;
+  int max_mana_points_;
+  int cur_mana_points_;
+  int max_damage_points_;
+  int cur_damage_points_;
+  int max_visibility_points_;
+  int cur_visibility_points_;
+  int max_level_points_;
+  int cur_level_points_;
 };
 
-class Zombie : public stats::ZombieStat, public Actor {
+class Hero : public ActiveActor {
  public:
-  Zombie() = default;
-  explicit Zombie(std::shared_ptr<stats::ZombieStat>, Point);
-  void collide(Actor&) override;
-  void collide(Hero&) override;
-  void collide(Zombie&) override;
-  void collide(Wall&) override;
-  void collide(Princess&) override;
-  void collide(Dragon&) override;
-  std::string sayName() const override;
-  enums::ActorID sayID() const override;
+  Hero(Point position, bool is_dead, bool is_immortal, int hp, int mp, int dp, int vp, int lp, int sp)
+      : ActiveActor(position, is_dead, is_immortal, "Hero", enums::HERO_ID, hp, mp, dp, vp, lp),
+        max_score_points{sp},
+        cur_score_points{sp} {};
+
+  enums::CollideResult collide(ActiveActor&) override;
+
  private:
-  const std::string NAME = "Zombie";
-  const enums::ActorID ID = enums::ZOMBIE_ID;
+  int max_score_points;
+  int cur_score_points;
 };
 
-class Wall : public stats::WallStat, public Actor {
+class Zombie : public ActiveActor {
  public:
-  Wall() = default;
-  explicit Wall(std::shared_ptr<stats::WallStat>, Point);
-  void collide(Actor&) override;
-  void collide(Hero&) override;
-  void collide(Zombie&) override;
-  void collide(Wall&) override;
-  void collide(Princess&) override;
-  void collide(Dragon&) override;
-  std::string sayName() const override;
-  enums::ActorID sayID() const override;
- private:
-  const std::string NAME = "Wall";
-  const enums::ActorID ID = enums::WALL_ID;
+  Zombie(Point position, bool is_dead, bool is_immortal, int hp, int mp, int dp, int vp, int lp, int sp)
+      : ActiveActor(position, is_dead, is_immortal, "Zombie", enums::ZOMBIE_ID, hp, mp, dp, vp, lp) {};
+
+  enums::CollideResult collide(ActiveActor&) override;
 };
 
-class Princess : public stats::PrincessStat, public Actor {
+class Dragon : public ActiveActor {
  public:
-  Princess() = default;
-  explicit Princess(std::shared_ptr<stats::PrincessStat>, Point);
-  void collide(Actor&) override;
-  void collide(Hero&) override;
-  void collide(Zombie&) override;
-  void collide(Wall&) override;
-  void collide(Princess&) override;
-  void collide(Dragon&) override;
-  std::string sayName() const override;
-  enums::ActorID sayID() const override;
- private:
-  const std::string NAME = "Princess";
-  const enums::ActorID ID = enums::PRINCESS_ID;
+  Dragon(Point position, bool is_dead, bool is_immortal, int hp, int mp, int dp, int vp, int lp, int sp)
+      : ActiveActor(position, is_dead, is_immortal, "Dragon", enums::DRAGON_ID, hp, mp, dp, vp, lp) {};
+
+  enums::CollideResult collide(ActiveActor&) override;
 };
 
-class Dragon : public stats::DragonStat, public Actor {
+class Princess : public ActiveActor {
  public:
-  Dragon() = default;
-  explicit Dragon(std::shared_ptr<stats::DragonStat>, Point);
-  void collide(Actor&) override;
-  void collide(Hero&) override;
-  void collide(Zombie&) override;
-  void collide(Wall&) override;
-  void collide(Princess&) override;
-  void collide(Dragon&) override;
-  std::string sayName() const override;
-  enums::ActorID sayID() const override;
- private:
-  const std::string NAME = "Dragon";
-  const enums::ActorID ID = enums::DRAGON_ID;
+  Princess(Point position, bool is_dead, bool is_immortal, int hp, int mp, int dp, int vp, int lp, int sp)
+      : ActiveActor(position, is_dead, is_immortal, "Princess", enums::PRINCESS_ID, hp, mp, dp, vp, lp) {};
+
+  enums::CollideResult collide(ActiveActor&) override;
 };
-//
-//class HealthPotion: public Actor {
-// public:
-//  HealthPotion() = default;
-//  explicit HealthPotion(std::shared_ptr<stats::DragonStat>, Point);
-//  void collide(Actor&) override;
-//  void collide(Hero&) override;
-//  void collide(Zombie&) override;
-//  void collide(Wall&) override;
-//  void collide(Princess&) override;
-//  void collide(Dragon&) override;
-//  std::string sayName() const override;
-// private:
-//  const std::string NAME = "Dragon";
-//};
+
+class SpellActor : public ActiveActor {
+ public:
+  SpellActor(Point position, bool is_dead, bool is_immortal, std::string name, enums::ActorID id,
+             int hp, int mp, int dp, int vp, int lp, Point direction)
+      : ActiveActor(position, is_dead, is_immortal, std::move(name), id, hp, mp, dp, vp, lp),
+        direction_{direction} {};
+
+  virtual enums::CollideResult collide(ActiveActor&) = 0;
+
+  void setDirection(Point);
+  Point getDirection() const;
+
+ protected:
+  Point direction_;
+};
+
+class Fireball : public SpellActor {
+  Fireball(Point position, bool is_dead, bool is_immortal, int hp, int mp, int dp, int vp, int lp, Point direction)
+      : SpellActor(position, is_dead, is_immortal, "Fireball", enums::FIRE_BALL_ID, hp, mp, dp, vp, lp, direction) {};
+
+  enums::CollideResult collide(ActiveActor&) override;
+};
+
+class PassiveActor : public Actor {
+ public:
+  PassiveActor(Point position, bool is_immortal, std::string name, enums::ActorID id)
+      : Actor(position, is_immortal, std::move(name), id) {};
+  ~PassiveActor() override = default;
+
+  enums::CollideResult collide(Actor&) override;
+  virtual enums::CollideResult collide(ActiveActor&) = 0;
+  enums::CollideResult collide(PassiveActor&) override;
+  enums::CollideResult collide(SpellActor&) override;
+  enums::CollideResult collide(CollectableActor&) override;
+};
+
+class Floor : public PassiveActor {
+ public:
+  Floor(Point position, bool is_immortal)
+      : PassiveActor(position, is_immortal, "Floor", enums::FLOOR_ID) {};
+
+  enums::CollideResult collide(ActiveActor&) override;
+};
+
+class Wall : public PassiveActor {
+ public:
+  Wall(Point position)
+      : PassiveActor(position, true, "Wall", enums::WALL_ID) {};
+
+  enums::CollideResult collide(ActiveActor&) override;
+};
+
+class CollectableActor : public PassiveActor {
+ public:
+  CollectableActor(Point position, bool is_immortal, std::string name, enums::ActorID id, int hp, int mp)
+      : PassiveActor(position, is_immortal, std::move(name), id),
+        max_health_points_{hp}, cur_health_points_{hp},
+        max_mana_points_{mp}, cur_mana_points_{mp} {};
+
+  virtual enums::CollideResult collide(ActiveActor&) = 0;
+
+ protected:
+  int max_health_points_;
+  int cur_health_points_;
+  int max_mana_points_;
+  int cur_mana_points_;
+};
+
+class HealthPotion : public CollectableActor {
+  HealthPotion(Point position, int hp, int mp)
+      : CollectableActor(position, true, "Health potion", enums::HP_POTION_ID, hp, mp) {}
+
+  enums::CollideResult collide(ActiveActor&) override;
+};
+
+class ManaPotion : public CollectableActor {
+  ManaPotion(Point position, int hp, int mp)
+      : CollectableActor(position, true, "Mana potion", enums::MP_POTION_ID, hp, mp) {}
+
+  enums::CollideResult collide(ActiveActor&) override;
+};
 
 } // namespace actor
-
-#endif //ROGUELIKE_ACTOR_H
