f9089f436f2dac41bfdc9be4509668d11abfc91f
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-07-10 06:51:28 +1000

    Implement everything except move methods of ActiveActors (but Hero) and PassiveActors

--- source/Actors.cpp
+++ source/Actors.cpp
@@ -6,260 +6,363 @@
 #include <cmath>
 #include <cassert>
 #include <utility>
+#include <ncurses.h>
 
-std::shared_ptr<actor::Actor> actor::Actor::createActor(enums::ActorID id, config::Config& config, Point coord) {
-  std::shared_ptr<actor::Actor> actor_ptr;
+void actor::Actor::setPosition(Point position) {
+  this->pos_ = position;
+}
 
-  switch (id) {
-    case enums::ActorID::HERO_ID:
-      actor_ptr = std::make_shared<actor::Hero>(actor::Hero(config.getHeroStat(), coord));
-      break;
-    case enums::ActorID::ZOMBIE_ID:
-      actor_ptr = std::make_shared<actor::Zombie>(actor::Zombie(config.getZombieStat(), coord));
-      break;
-    case enums::ActorID::WALL_ID:
-      actor_ptr = std::make_shared<actor::Wall>(actor::Wall(config.getWallStat(), coord));
-      break;
-    case enums::ActorID::PRINCESS_ID:
-      actor_ptr = std::make_shared<actor::Princess>(actor::Princess(config.getPrincessStat(), coord));
-      break;
-    case enums::ActorID::DRAGON_ID:
-      actor_ptr = std::make_shared<actor::Dragon>(actor::Dragon(config.getDragonStat(), coord));
-      break;
-    case enums::ActorID::NOTHING_ID:
-      actor_ptr = nullptr;
-      break;
-    default:
-      assert(false);
+Point actor::Actor::getPosition() const {
+  return this->pos_;
+}
+
+void actor::Actor::setSymbol(char symbol) {
+  symbol_ = symbol;
+}
+
+char actor::Actor::getSymbol() const {
+  return symbol_;
+}
+
+void actor::Actor::setIsImmortal(bool is_immortal) {
+  is_immortal_ = is_immortal;
+}
+
+bool actor::Actor::getIsImmortal() const {
+  return is_immortal_;
+}
+
+std::string actor::Actor::getName() const {
+  return name_;
+}
+
+enums::ActorID actor::Actor::getID() const {
+  return id_;
+}
+
+enums::CollideResult actor::ActiveActor::collide(Actor& other) {
+  return other.collide(*this);
+}
+
+enums::CollideResult actor::ActiveActor::collide(PassiveActor& other) {
+  return other.collide(*this);
+}
+
+enums::CollideResult actor::ActiveActor::collide(SpellActor& other) {
+  return other.collide(*this);
+}
+
+enums::CollideResult actor::ActiveActor::collide(actor::CollectableActor& other) {
+  return other.collide(*this);
+}
+
+void actor::ActiveActor::setIsDead(bool is_dead) {
+  is_dead_ = is_dead;
+}
+
+bool actor::ActiveActor::getIsDead() const {
+  return is_dead_;
+}
+
+void actor::ActiveActor::setMaxHealthPoints(int hp) {
+  max_health_points_ = hp;
+}
+
+int actor::ActiveActor::getMaxHealthPoints() const {
+  return max_health_points_;
+}
+
+void actor::ActiveActor::setCurHealthPoints(int hp) {
+  if (!this->is_dead_) {
+    cur_health_points_ = hp % max_health_points_;
   }
-  return actor_ptr;
 }
 
-actor::Hero::Hero(std::shared_ptr<stats::HeroStat> new_st, Point coord) {
-//  std::shared_ptr<stats::HeroStat> new_st = std::dynamic_pointer_cast<stats::HeroStat>(st);
-  this->setMaxHP(new_st->getMaxHP());
-  this->setCurHP(new_st->getCurHP());
-  this->setMaxMP(new_st->getMaxMP());
-  this->setCurMP(new_st->getCurMP());
-  this->setMaxDP(new_st->getMaxDP());
-  this->setCurDP(new_st->getCurDP());
-  this->setMaxVP(new_st->getMaxVP());
-  this->setCurVP(new_st->getCurVP());
-  this->setDead(new_st->isDead());
-  this->setImmortal(new_st->isImmortal());
-  this->setDeadSymbol(new_st->getDeadSymbol());
-  this->setUndeadSymbol(new_st->getUndeadSymbol());
-  this->setCoord(coord);
+int actor::ActiveActor::getCurHealthPoints() const {
+  return cur_health_points_;
 }
 
-void actor::Hero::collide(actor::Actor& _ca) {
-  _ca.collide(*this);
+void actor::ActiveActor::setMaxManaPoints(int mp) {
+  max_mana_points_ = mp;
 }
 
-void actor::Hero::collide(actor::Hero& _ch) {}
-
-void actor::Hero::collide(actor::Zombie& _cz) {
-  this->setCurHP(this->getCurHP() - _cz.getCurDP());
-  _cz.setCurHP(_cz.getCurHP() - this->getCurDP());
+int actor::ActiveActor::getMaxManaPoints() const {
+  return max_mana_points_;
 }
 
-void actor::Hero::collide(actor::Wall& _cw) {
-  _cw.setCurHP(_cw.getCurHP() - this->getCurDP());
+void actor::ActiveActor::setCurManaPoints(int mp) {
+  cur_mana_points_ = mp % max_mana_points_;
 }
 
-void actor::Hero::collide(actor::Princess&) {
-  //some win event
+int actor::ActiveActor::getCurManaPoints() const {
+  return cur_mana_points_;
 }
 
-void actor::Hero::collide(actor::Dragon& _cd) {
-  this->setCurHP(this->getCurHP() - _cd.getCurDP());
-  _cd.setCurHP(_cd.getCurHP() - this->getCurDP());
+void actor::ActiveActor::setDamagePoints(int dp) {
+  damage_points_ = dp;
 }
 
-std::string actor::Hero::sayName() const {
-  return this->NAME;
+int actor::ActiveActor::getDamagePoints() const {
+  return damage_points_;
 }
 
-enums::ActorID actor::Hero::sayID() const {
-  return this->ID;
+void actor::ActiveActor::setVisibilityPoints(int vp) {
+  visibility_points_ = vp;
 }
 
-actor::Zombie::Zombie(std::shared_ptr<stats::ZombieStat> new_st, Point coord) {
-  this->setMaxHP(new_st->getMaxHP());
-  this->setCurHP(new_st->getCurHP());
-  this->setMaxDP(new_st->getMaxDP());
-  this->setCurDP(new_st->getCurDP());
-  this->setMaxVP(new_st->getMaxVP());
-  this->setCurVP(new_st->getCurVP());
-  this->setDead(new_st->isDead());
-  this->setImmortal(new_st->isImmortal());
-  this->setDeadSymbol(new_st->getDeadSymbol());
-  this->setUndeadSymbol(new_st->getUndeadSymbol());
-  this->setCoord(coord);
+int actor::ActiveActor::getVisibilityPoints() const {
+  return visibility_points_;
 }
 
-void actor::Zombie::collide(actor::Actor& _ca) {
-  _ca.collide(*this);
+void actor::ActiveActor::upLevelPoints() {
+  ++level_points_;
 }
 
-void actor::Zombie::collide(actor::Hero& _ch) {
-  _ch.collide(*this);
+void actor::ActiveActor::downLevelPoints() {
+  if (level_points_ > 0) {
+    (--level_points_);
+  } else {
+    level_points_ = 1;
+  }
 }
 
-void actor::Zombie::collide(actor::Zombie& _cz) {
-  //do nothing
+int actor::ActiveActor::getLevelPoints() const {
+  return level_points_;
 }
 
-void actor::Zombie::collide(actor::Wall& _cv) {
-  //do nothing
+void actor::ActiveActor::setCurScorePoints(int sp) {
+  cur_score_points_ = sp;
+  while (cur_score_points_ >= max_score_points_) {
+    this->upLevelPoints();
+    this->setMaxHealthPoints(max_health_points_ + max_health_points_ * level_points_ / 10);
+    this->setCurHealthPoints(max_health_points_);
+    this->setMaxManaPoints(max_mana_points_ + max_mana_points_ * level_points_ / 10);
+    this->setCurManaPoints(max_mana_points_);
+    this->setMaxScorePoints(max_score_points_ + max_score_points_ * level_points_ / 10);
+    cur_score_points_ -= max_score_points_;
+  }
 }
 
-void actor::Zombie::collide(actor::Princess&) {
-  //do nothing
+int actor::ActiveActor::getCurScorePoints() const {
+  return cur_score_points_;
 }
 
-void actor::Zombie::collide(actor::Dragon&) {
-  //do nothing
+void actor::ActiveActor::setMaxScorePoints(int sp) {
+  max_score_points_ = sp;
 }
 
-std::string actor::Zombie::sayName() const {
-  return this->NAME;
+int actor::ActiveActor::getMaxScorePoints() const {
+  return max_score_points_;
 }
 
-enums::ActorID actor::Zombie::sayID() const {
-  return this->ID;
+enums::CollideResult actor::Hero::collide(ActiveActor& other) {
+  enums::CollideResult result = enums::BARRIER;
+  enums::ActorID other_id = other.getID();
+  if (other.getIsDead()) {
+    result = enums::FREE;
+  } else if (other_id == enums::HERO_ID) {
+    result = enums::BARRIER;
+  } else if ((other_id == enums::ZOMBIE_ID) or (other_id == enums::DRAGON_ID)) {
+    result = enums::FIGHT;
+  } else if (other_id == enums::PRINCESS_ID) {
+    result = enums::WIN;
+  }
+  return result;
 }
 
-actor::Wall::Wall(std::shared_ptr<stats::WallStat> st, Point coord) {
-  this->setMaxHP(st->getMaxHP());
-  this->setCurHP(st->getCurHP());
-  this->setDead(st->isDead());
-  this->setImmortal(st->isImmortal());
-  this->setDeadSymbol(st->getDeadSymbol());
-  this->setUndeadSymbol(st->getUndeadSymbol());
-  this->setCoord(coord);
+enums::CollideResult actor::Hero::move() {
+  Point hero_pos = this->getPosition();
+  auto area = game_manager_.getArea();
+  int key = game_manager_.getLastPressedKey();
+  Point dir = {0, 0};
+
+  if (key == KEY_UP) {
+    dir.x -= 1;
+  } else if (key == KEY_DOWN) {
+    dir.x += 1;
+  } else if (key == KEY_LEFT) {
+    dir.y -= 1;
+  } else if (key == KEY_RIGHT) {
+    dir.y += 1;
+  }
+
+  Point other_pos = {hero_pos.x + dir.x, hero_pos.y + dir.y};
+  std::shared_ptr<Actor> other = area[other_pos.x][other_pos.y];
+  enums::CollideResult collision = this->collide(*this);
+
+  switch (collision) {
+    case enums::BARRIER:
+      break;
+    case enums::FREE:
+      game_manager_.swap(hero_pos, other_pos);
+      break;
+    case enums::FIGHT:
+      std::shared_ptr<actor::ActiveActor> enemy = std::static_pointer_cast<actor::ActiveActor>(other);
+      enemy->setCurHealthPoints(enemy->getCurHealthPoints() - this->getDamagePoints());
+      this->setCurScorePoints(enemy->level_points_ * 10);
+      if (enemy->getIsDead()) {
+        game_manager_.swap(hero_pos, other_pos);
+      } else {
+        this->setCurHealthPoints(this->getCurHealthPoints() - enemy->getDamagePoints());
+        enemy->setCurScorePoints(this->level_points_ * 10);
+      }
+    case enums::WIN:
+      //do nothing
+      break;
+    case enums::PICK:
+      if (other->getID() == enums::HP_POTION_ID)
+        this->setCurHealthPoints(this->getCurHealthPoints() + this->getMaxHealthPoints() * this->getLevelPoints() / 10);
+      else {
+        this->setCurManaPoints(this->getCurManaPoints() + this->getMaxManaPoints() * this->getLevelPoints() / 10);
+      }
+  }
+
+  return collision;
 }
 
-void actor::Wall::collide(actor::Actor& _ca) {
-  _ca.collide(*this);
+enums::CollideResult actor::Zombie::collide(ActiveActor& other) {
+  enums::CollideResult result = enums::BARRIER;
+  enums::ActorID other_id = other.getID();
+  if (other.getIsDead()) {
+    result = enums::PICK;
+  } else if ((other_id == enums::HERO_ID)) {
+    result = enums::FIGHT;
+  } else if ((other_id == enums::ZOMBIE_ID) or (other_id == enums::DRAGON_ID) or (other_id == enums::PRINCESS_ID)) {
+    result = enums::BARRIER;
+  }
+  return result;
 }
 
-void actor::Wall::collide(actor::Hero& _ch) {
-  _ch.collide(*this);
+enums::CollideResult actor::Dragon::collide(ActiveActor& other) {
+  enums::CollideResult result = enums::BARRIER;
+  enums::ActorID other_id = other.getID();
+  if ((other_id == enums::HERO_ID)) {
+    result = enums::FIGHT;
+  } else if ((other_id == enums::ZOMBIE_ID) or (other_id == enums::DRAGON_ID) or (other_id == enums::PRINCESS_ID)) {
+    result = enums::BARRIER;
+  } else if (other.getIsDead()) {
+    result = enums::FREE;
+  }
+  return result;
 }
 
-void actor::Wall::collide(actor::Zombie& _cz) {
-  _cz.collide(*this);
+enums::CollideResult actor::Princess::collide(ActiveActor& other) {
+  enums::CollideResult result = enums::BARRIER;
+  enums::ActorID other_id = other.getID();
+  if ((other_id == enums::FIRE_BALL_ID) or (other_id == enums::ZOMBIE_ID) or (other_id == enums::DRAGON_ID)) {
+    result = enums::FIGHT;
+  } else if ((other.getIsDead()) or (other_id == enums::PRINCESS_ID)) {
+    result = enums::BARRIER;
+  } else if (other_id == enums::HERO_ID) {
+    result = enums::WIN;
+  }
+  return result;
 }
 
-void actor::Wall::collide(actor::Wall&) {
-  //do nothing
+void actor::SpellActor::setDirection(Point dir) {
+  direction_ = dir;
 }
 
-void actor::Wall::collide(actor::Princess&) {
-  //do nothing
+Point actor::SpellActor::getDirection() const {
+  return direction_;
 }
 
-void actor::Wall::collide(actor::Dragon&) {
-  //do nothing
+enums::CollideResult actor::Fireball::collide(actor::ActiveActor& other) {
+  enums::CollideResult result = enums::BARRIER;
+  enums::ActorID other_id = other.getID();
+  if (other.getIsDead()) {
+    result = enums::FREE;
+  } else if ((other_id == enums::ZOMBIE_ID) or (other_id == enums::PRINCESS_ID) or (other_id == enums::HERO_ID)) {
+    result = enums::FIGHT;
+  } else if (other_id == enums::DRAGON_ID) {
+    result = enums::PICK;
+  }
+  return result;
 }
 
-std::string actor::Wall::sayName() const {
-  return this->NAME;
+enums::CollideResult actor::PassiveActor::collide(Actor& other) {
+  return other.collide(*this);
 }
 
-enums::ActorID actor::Wall::sayID() const {
-  return this->ID;
+enums::CollideResult actor::PassiveActor::collide(ActiveActor& other) {
+  enums::CollideResult result = enums::FREE;
+  enums::ActorID this_id = this->getID();
+  enums::ActorID other_id = other.getID();
+  if (this_id == enums::WALL_ID) {
+    result = enums::BARRIER;
+  } else if (this_id == enums::FLOOR_ID) {
+    result = enums::FREE;
+  }
+  return result;
 }
 
-actor::Princess::Princess(std::shared_ptr<stats::PrincessStat> new_st, Point coord) {
-  this->setMaxMP(new_st->getMaxMP());
-  this->setCurMP(new_st->getCurMP());
-  this->setMaxVP(new_st->getMaxVP());
-  this->setCurVP(new_st->getCurVP());
-  this->setDead(new_st->isDead());
-  this->setImmortal(new_st->isImmortal());
-  this->setDeadSymbol(new_st->getDeadSymbol());
-  this->setUndeadSymbol(new_st->getUndeadSymbol());
-  this->setCoord(coord);
+enums::CollideResult actor::PassiveActor::collide(PassiveActor& other) {
+  return enums::BARRIER;
 }
 
-void actor::Princess::collide(actor::Actor& _ca) {
-  _ca.collide(*this);
+enums::CollideResult actor::PassiveActor::collide(SpellActor& other) {
+  enums::CollideResult result = enums::FREE;
+  enums::ActorID this_id = this->getID();
+  if (this_id == enums::WALL_ID) {
+    result = enums::BARRIER;
+  } else if ((this_id == enums::FLOOR_ID) or (this_id == enums::HP_POTION_ID) or (this_id == enums::MP_POTION_ID)) {
+    result = enums::FREE;
+  }
+  return result;
 }
 
-void actor::Princess::collide(actor::Hero& _ch) {
-  _ch.collide(*this);
+enums::CollideResult actor::PassiveActor::collide(CollectableActor& other) {
+  enums::CollideResult result = enums::FREE;
+  enums::ActorID this_id = this->getID();
+  if (this_id == enums::WALL_ID) {
+    result = enums::BARRIER;
+  } else if ((this_id == enums::FLOOR_ID) or (this_id == enums::HP_POTION_ID) or (this_id == enums::MP_POTION_ID)) {
+    result = enums::FREE;
+  }
+  return result;
 }
 
-void actor::Princess::collide(actor::Zombie& _cz) {
-  _cz.collide(*this);
+enums::CollideResult actor::CollectableActor::collide(ActiveActor& other) {
+  enums::CollideResult result = enums::FREE;
+  enums::ActorID this_id = this->getID();
+  enums::ActorID other_id = other.getID();
+  if ((this_id == enums::HP_POTION_ID) or (this_id == enums::MP_POTION_ID)) {
+    if ((other_id == enums::HERO_ID) or (other_id == enums::PRINCESS_ID)) {
+      result = enums::PICK;
+    } else {
+      result = enums::FREE;
+    }
+  }
+  return result;
 }
 
-void actor::Princess::collide(actor::Wall& _cw) {
-  _cw.collide(*this);
-}
 
-void actor::Princess::collide(actor::Princess&) {
-  //lol it's not possible
-}
-
-void actor::Princess::collide(actor::Dragon&) {
-  //do nothing
-}
-
-std::string actor::Princess::sayName() const {
-  return this->NAME;
-}
-
-enums::ActorID actor::Princess::sayID() const {
-  return this->ID;
-}
-
-actor::Dragon::Dragon(std::shared_ptr<stats::DragonStat> new_st, Point coord) {
-  this->setMaxHP(new_st->getMaxHP());
-  this->setCurHP(new_st->getCurHP());
-  this->setMaxMP(new_st->getMaxMP());
-  this->setCurMP(new_st->getCurMP());
-  this->setMaxDP(new_st->getMaxDP());
-  this->setCurDP(new_st->getCurDP());
-  this->setMaxVP(new_st->getMaxVP());
-  this->setCurVP(new_st->getCurVP());
-  this->setDead(new_st->isDead());
-  this->setImmortal(new_st->isImmortal());
-  this->setDeadSymbol(new_st->getDeadSymbol());
-  this->setUndeadSymbol(new_st->getUndeadSymbol());
-  this->setCoord(coord);
-}
-
-void actor::Dragon::collide(actor::Actor& _ca) {
-  _ca.collide(*this);
-}
-
-void actor::Dragon::collide(actor::Hero& _ch) {
-  _ch.collide(*this);
-}
-
-void actor::Dragon::collide(actor::Zombie& _cz) {
-  _cz.collide(*this);
-}
-
-void actor::Dragon::collide(actor::Wall& _cw) {
-  _cw.collide(*this);
-}
-
-void actor::Dragon::collide(actor::Princess& _cp) {
-  _cp.collide(*this);
-}
-
-void actor::Dragon::collide(actor::Dragon&) {
-  //do nothing
-}
-
-std::string actor::Dragon::sayName() const {
-  return this->NAME;
-}
-
-enums::ActorID actor::Dragon::sayID() const {
-  return this->ID;
-}
+//std::shared_ptr<actor::ActiveActor> actor::ActiveActor::createActor(enums::ActorID id,
+//                                                                    config::Config& config,
+//                                                                    Point coord) {
+//  std::shared_ptr<actor::ActiveActor> actor_ptr;
+//
+//  switch (id) {
+//    case enums::ActorID::HERO_ID:
+//      actor_ptr = std::make_shared<actor::Hero>(actor::Hero(config.getHeroStat(), coord));
+//      break;
+//    case enums::ActorID::ZOMBIE_ID:
+//      actor_ptr = std::make_shared<actor::Zombie>(actor::Zombie(config.getZombieStat(), coord));
+//      break;
+//    case enums::ActorID::WALL_ID:
+//      actor_ptr = std::make_shared<actor::Wall>(actor::Wall(config.getWallStat(), coord));
+//      break;
+//    case enums::ActorID::PRINCESS_ID:
+//      actor_ptr = std::make_shared<actor::Princess>(actor::Princess(config.getPrincessStat(), coord));
+//      break;
+//    case enums::ActorID::DRAGON_ID:
+//      actor_ptr = std::make_shared<actor::Dragon>(actor::Dragon(config.getDragonStat(), coord));
+//      break;
+//    case enums::ActorID::NOTHING_ID:
+//      actor_ptr = nullptr;
+//      break;
+//    default:
+//      assert(false);
+//  }
+//  return actor_ptr;
+//}
