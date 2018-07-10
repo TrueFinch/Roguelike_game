bfad55241dfa4db1cc3b51820da40ee91a844aa8
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-07-11 04:25:53 +1000

    Add factory class for actors. Without implementation now

---
+++ include/ActorFactory.h
@@ -0,0 +1,23 @@
+//
+// Created by truefinch on 11.07.18.
+//
+#pragma once
+
+#include <Actors.h>
+#include <Hero.h>
+#include <Zombie.h>
+#include <Dragon.h>
+#include <Princess.h>
+#include <Fireball.h>
+#include <Wall.h>
+#include <Floor.h>
+#include <HealthPotion.h>
+#include <ManaPotion.h>
+
+namespace factory {
+
+class ActorFactory {
+
+};
+
+} // namespace factory
